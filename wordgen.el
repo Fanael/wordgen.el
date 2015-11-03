6230345a52b79fa027d3d91cb3f5c04552b0f55b
Fanael Linithien <fanael4@gmail.com>
2015-11-03 20:20:01 +0100

    Paste Emacs Lisp forms together when compiling
    instead of creating zillions of closures.

--- wordgen.el
+++ wordgen.el
@@ -50,31 +50,40 @@
     result))
 
 (defun wordgen-compile-ruleset (ruleset)
-  "Compile RULESET to executable form."
+  "Compile RULESET to executable form.
+RULESET should be a rule set of the same form as in `wordgen'."
   (let ((rules (make-hash-table :test #'eq)))
     (dolist (rule ruleset)
       (pcase rule
         (`(,rule-name ,rule-expr)
          (when (gethash rule-name rules)
            (error "Redefinition of rule %S" rule-name))
-         (puthash rule-name (wordgen--compile-expression rule-expr) rules))
+         (puthash rule-name (wordgen--compile-rule-body rule-expr) rules))
         (_
          (error "Invalid rule %S" rule))))
     rules))
 
 (defvar wordgen--output-strings '())
 
-(defmacro wordgen--with-output-to-string (&rest body)
-  ""
+(defmacro wordgen-with-output-to-string (&rest body)
+  "Execute BODY, returning the strings passed to `wordgen-print-string'.
+The strings are concantenated in the order of the `wordgen-print-string' calls."
   (declare (debug t) (indent defun))
   `(let ((wordgen--output-strings '()))
      (progn ,@body)
      (apply #'concat (nreverse wordgen--output-strings))))
 
+(defun wordgen-print-string (string)
+  "Print a STRING.
+The string is printed to the innermost enclosing
+`wordgen-with-output-to-string'."
+  (push string wordgen--output-strings)
+  'string)
+
 (defun wordgen-evaluate-ruleset (ruleset rng &optional starting-rule)
   "Evaluate the RULESET using pseudo-random generator RNG.
 Evaluation starts from STARTING-RULE; if that's nil, it starts from `result'."
-  (wordgen--with-output-to-string
+  (wordgen-with-output-to-string
     (wordgen-call-rule-by-name ruleset rng (or starting-rule 'result))))
 
 (defun wordgen-call-rule-by-name (ruleset rng rule-name)
@@ -94,8 +103,33 @@ RNG is the pseudo-random number generator.
 RULE-DESC is the rule descriptor to call."
   (funcall rule-desc ruleset rng))
 
+;; Wordgen code is made executable by converting its expressions to Emacs Lisp
+;; forms, which then are joined together (which is trivial, as Emacs Lisp is a
+;; Lisp), put into a lambda, and finally compiled by Emacs's own byte compiler.
+;; It's done this way because it's probably the easiest way in Lisp and it's
+;; reasonably fast.
+
+(defvar wordgen--compile-to-bytecode t
+  "If non-nil, compile the generated lambdas to Emacs bytecode.
+Should be t at all times, expect when debugging.")
+
+(defun wordgen--compile-rule-body (expression)
+  "Compile rule body EXPRESSION to an Emacs Lisp function."
+  (let ((func `(lambda (rules rng)
+                 ,(wordgen--compile-expression expression))))
+    (if wordgen--compile-to-bytecode
+        ;; We have to silence `byte-compile-log-warning' as it can log some
+        ;; warnings to *Compile-Log* even though we set `byte-compile-warnings'
+        ;; to nil.
+        (cl-letf (((symbol-function #'byte-compile-log-warning) #'ignore)
+                  (lexical-binding t)
+                  (byte-compile-warnings nil))
+          (byte-compile func))
+      ;; Not compiling, just return the lambda.
+      func)))
+
 (defun wordgen--compile-expression (expression)
-  "Compile EXPRESSION to executable form."
+  "Compile EXPRESSION to an Emacs Lisp form."
   (pcase expression
     ((pred stringp) (wordgen--compile-string expression))
     ((pred integerp) (wordgen--compile-integer expression))
@@ -105,43 +139,39 @@ RULE-DESC is the rule descriptor to call."
     (_ (error "Invalid expression %S" expression))))
 
 (defun wordgen--compile-string (string)
-  "Compile STRING to executable form."
-  (lambda (_rules _rng)
-    (push string wordgen--output-strings)
-    'string))
+  "Compile STRING to an Emacs Lisp form."
+  `(wordgen-print-string ,string))
 
 (defun wordgen--compile-integer (integer)
-  "Compile INTEGER to executable form."
-  (lambda (_rules _rng)
-    integer))
+  "Compile INTEGER to an Emacs Lisp form."
+  integer)
 
 (defun wordgen--compile-choice-expr (vec)
-  "Compile choice expression VEC to executable form."
-  (let* ((len (length vec))
-         (compiled-subexprs (make-vector len nil))
-         (total-weight-so-far 0))
-    (dotimes (i len)
-      (pcase-let ((`(,weight . ,expr)
-                   (pcase (aref vec i)
-                     (`(,weight ,expr) (cons weight expr))
-                     (expr (cons 1 expr)))))
-        (setq total-weight-so-far (+ total-weight-so-far weight))
-        (aset compiled-subexprs i (cons total-weight-so-far (wordgen--compile-expression expr)))))
-    (let ((max-rolled-number total-weight-so-far))
-      (lambda (rules rng)
-        (let ((rolled-number (wordgen--prng-next-int max-rolled-number rng)))
-          (catch 'break
-            (let ((i 0))
-              ;; We could use binary search here, but the typical number of
-              ;; choices is too small for it to be worthwhile.
-              (while t
-                (let ((element (aref compiled-subexprs i)))
-                  (setq i (1+ i))
-                  (when (<= rolled-number (car element))
-                    (throw 'break (funcall (cdr element) rules rng))))))))))))
+  "Compile choice expression VEC to an Emacs Lisp form.
+
+Currently it's done by turning it into cond, comparing the rolled number against
+consecutive running total weights."
+  ;; TODO: consider compiling to jump tables where appropriate.
+  (let* ((total-weight-so-far 0)
+         (cond-clauses
+          (mapcar (lambda (subexpr)
+                    (let ((weight 1)
+                          (expr subexpr))
+                      (pcase subexpr
+                        ((and `(,subexpr-weight ,subexpr-expr)
+                              (guard (integerp subexpr-weight)))
+                         (setq weight subexpr-weight)
+                         (setq expr subexpr-expr)))
+                      (setq total-weight-so-far (+ total-weight-so-far weight))
+                      `((< rolled-number ,total-weight-so-far)
+                        ,(wordgen--compile-expression expr))))
+                  vec)))
+    `(let ((rolled-number (wordgen--prng-next-int ,(1- total-weight-so-far) rng)))
+       (cond
+        ,@cond-clauses))))
 
 (defun wordgen--compile-function-call (function-call)
-  "Compile a builtin FUNCTION-CALL to executable form."
+  "Compile a builtin FUNCTION-CALL to an Emacs Lisp form."
   (pcase function-call
     (`(++ . ,rest)
      (wordgen--compile-concat rest))
@@ -155,59 +185,64 @@ RULE-DESC is the rule descriptor to call."
      (error "Invalid function call expression %S" function-call))))
 
 (defun wordgen--compile-concat (expressions)
-  "Compile concatenation to executable form.
-EXPRESSIONS is a list of subexpressions."
-  (let* ((len (length expressions))
-         (compiled-subexprs (make-vector len nil))
-         (i 0))
-    (dolist (expr expressions)
-      (aset compiled-subexprs i (wordgen--compile-expression expr))
-      (setq i (1+ i)))
-    (lambda (rules rng)
-      (dotimes (i len)
-        (unless (eq 'string (funcall (aref compiled-subexprs i) rules rng))
-          (error "Arguments of ++ must be strings")))
-      'string)))
+  "Compile concatenation to an Emacs Lisp form.
+EXPRESSIONS is a list of subexpressions.
+
+As all EXPRESSION must evaluate to a string, and we concat strings by adding
+them to a list, this is just a `progn'."
+  `(progn
+     ,@(mapcar (lambda (expr)
+                 `(unless (eq 'string ,(wordgen--compile-expression expr))
+                    (error "Arguments of ++ must be strings")))
+               expressions)))
 
 (defun wordgen--compile-replicate (times expr)
-  ""
+  "Compile a replicate expression to an Emacs Lisp form.
+
+TIMES is the uncompiled replication count subexpression.
+EXPR is the uncompiled subexpression evaluating to the string to replicate.
+If TIMES is <= 0, the whole replicate evaluates to an empty string."
   (let ((times-compiled (wordgen--compile-expression times))
         (expr-compiled (wordgen--compile-expression expr)))
-    (lambda (rules rng)
-      (let ((actual-times (funcall times-compiled rules rng)))
-        (unless (integerp actual-times)
-          (error "Replication count %S is not an integer" actual-times))
-        (when (> actual-times 0)
-          (let ((string
-                 (wordgen--with-output-to-string
-                   (unless (eq 'string (funcall expr-compiled rules rng))
-                     (error "Second argument of replicate must be a string")))))
-            (dotimes (_ actual-times)
-              (push string wordgen--output-strings)))))
-      'string)))
+    `(let ((times ,times-compiled))
+       (unless (integerp times)
+         (error "Replication count %S is not an integer" times))
+       (when (> times 0)
+         (let ((string
+                (wordgen-with-output-to-string
+                  (unless (eq 'string ,expr-compiled)
+                    (error "Second argument of replicate must be a string")))))
+           (dotimes (_ times)
+             (push string wordgen--output-strings))))
+       'string)))
 
 (defun wordgen--compile-eval-multiple-times (times expr)
-  ""
+  "Compile an eval-multiple-times expression to an Emacs Lisp form.
+
+TIMES is the uncompiled replication count subexpression.
+EXPR is the uncompiled subexpression to evaluate multiple times.
+Only the result of the last evaluation is returned.
+If TIMES is <= 0, the whole eval-multiple-times evaluates to an empty string."
   (let ((times-compiled (wordgen--compile-expression times))
         (expr-compiled (wordgen--compile-expression expr)))
-    (lambda (rules rng)
-      (let ((actual-times (funcall times-compiled rules rng)))
-        (unless (integerp actual-times)
-          (error "Replication count %S is not an integer" actual-times))
-        (if (<= actual-times 0)
-            'string
-          (dotimes (_ (1- actual-times))
-            (funcall expr-compiled rules rng))
-          (funcall expr-compiled rules rng))))))
+    `(let ((times ,times-compiled))
+       (unless (integerp times)
+         (error "Evaluation count %S is not an integer" times))
+       (if (<= times 0)
+           'string
+         ;; Run the loop N-1 times, so the result of the if will be the same as
+         ;; that of last expr evaluation.
+         (dotimes (_ (1- times))
+           ,expr-compiled)
+         ,expr-compiled))))
 
 (defun wordgen--compile-call-lisp (lisp-function)
-  ""
-  lisp-function)
+  "Compile a call to a LISP-FUNCTION."
+  `(funcall ,lisp-function rules rng))
 
 (defun wordgen--compile-rule-call (rule-name)
   "Compile a call to rule RULE-NAME."
-  (lambda (rules rng)
-    (wordgen-call-rule-by-name rules rng rule-name)))
+  `(wordgen-call-rule-by-name rules rng ',rule-name))
 
 (defun wordgen--get-default-seed ()
   "Get the PRNG default seed.
